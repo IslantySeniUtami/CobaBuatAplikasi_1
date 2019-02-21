97be617dee0bdaf58acce68573070a22668135af
IslantySeniUtami <46374176+IslantySeniUtami@users.noreply.github.com>
2019-02-21 09:22:46 +0700

    Add files via upload

---
+++ CobaBuatAplikasi.java
@@ -0,0 +1,28 @@
+package CobaBuatAplikasi;
+import java.util.Scanner;
+public class CobaBuatAplikasi {
+    public static void main(String[] args) {
+        
+        Scanner baca = new Scanner(System.in);
+        double nomer;
+        
+        
+        System.out.print("Tekan tombol power: ");
+        double nomor =  baca.nextDouble();
+        
+        if (nomor == 15)
+        {
+            System.out.println("Lampu menyala");
+        }
+        else 
+        {
+            System.out.println("Lampu mati");
+        }
+        
+        
+                
+                
+        
+    }
+    
+}
